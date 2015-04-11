ee84ed83c13e593db2e4e23597f7ff14a85f773b
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2015-04-11 22:56:32 +0300

    Make compatible to new FRP desing

--- moorka-todomvc/src/main/scala/MoorkaTodoMVC.scala
+++ moorka-todomvc/src/main/scala/MoorkaTodoMVC.scala
@@ -1,12 +1,10 @@
 
-import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
-
 import moorka.rx._
 import moorka.ui._
-import moorka.ui.components.html._
 import moorka.ui.components.base._
-import scala.scalajs.js
-import js.Dynamic.global
+import moorka.ui.components.html._
+
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
 
 object MoorkaTodoMVC extends Application {
   println("MoorkaTodoMVC constructed")
@@ -21,15 +19,20 @@ object MoorkaTodoMVC extends Application {
       case (num, Todo(_, Completed)) => num + 1
       case (num, _) => num
     }
-    val numActive = Bind { todos.length() - numCompleted() }
+    val numActive = for { 
+      todosLength ← todos.rxLength
+      n ← numCompleted 
+    }
+    yield {
+      todosLength - n
+    } 
     val nowEditing = Var(false)
     val filter = Var[Filter](All)
     // Internal component. It has own state but depends on
     // display state of main component
-    class TodoComponent(state: State[Todo]) extends Component {
+    class TodoComponent(state: Rx[Todo]) extends Component {
       val el = {
         val fieldText = Var("")
-        state.observe(fieldText() = state().txt)
         li(
           useClassName("completed") := state.map(_.status == Completed),
           useClassName("editing") := state.map(_.status == Editing),
@@ -40,10 +43,14 @@ object MoorkaTodoMVC extends Application {
           },
           div(`className` := "view",
             on.`double-click` listen {
-              val todo = state()
-              if (todo.status == Active && !nowEditing()) {
-                todos.updateElement(todo, todo.copy(status = Editing))
-                nowEditing() = true
+              nowEditing modOnce { nowEditing ⇒
+                state map { todo ⇒
+                  if (todo.status == Active && !nowEditing) {
+                    todos.updateElement(todo, todo.copy(status = Editing))
+                    true
+                  }
+                  else nowEditing
+                }
               }
             },
             input(
@@ -51,13 +58,14 @@ object MoorkaTodoMVC extends Application {
               `type` := "checkbox",
               `style` := "cursor: pointer",
               on.click listen {
-                val todo = state()
-                val newStatus = todo.status match {
-                  case Active => Completed
-                  case Completed => Active
-                  case Editing => Active
+                state once { todo ⇒
+                  val newStatus = todo.status match {
+                    case Active => Completed
+                    case Completed => Active
+                    case Editing => Active
+                  }
+                  todos.updateElement(todo, todo.copy(status = newStatus))
                 }
-                todos.updateElement(todo, todo.copy(status = newStatus))
               },
               `checked` := state.map(_.status == Completed)
             ),
@@ -66,15 +74,25 @@ object MoorkaTodoMVC extends Application {
               `className` := "destroy",
               `style` := "cursor: pointer",
               on.click listen {
-                todos -= state()
+                state once { todo ⇒
+                  todos -= todo
+                }
               }
             )
           ),
           form(
             on.submit listen {
-              val todo = state()
-              todos.updateElement(todo, Todo(txt = fieldText(), status = Active))
-              nowEditing() = false
+              fieldText once { fieldText ⇒
+                state once { todo ⇒
+                  nowEditing modOnce { _ ⇒
+                    todos.updateElement(todo, Todo(
+                      txt = fieldText,
+                      status = Active
+                    ))
+                    false
+                  }
+                }
+              }
             },
             input(`className` := "edit", `value` =:= fieldText)
           )
@@ -96,10 +114,12 @@ object MoorkaTodoMVC extends Application {
                   `value` =:= inputText
                 ),
                 on.submit listen {
-                  val s = inputText().trim()
-                  if (s != "") {
-                    todos += Todo(s, Active)
-                    inputText() = ""
+                  inputText modOnce { inputText ⇒
+                    val s = inputText.trim()
+                    if (s != "") {
+                      todos += Todo(s, Active)
+                    }
+                    ""
                   }
                 }
               )
@@ -110,7 +130,14 @@ object MoorkaTodoMVC extends Application {
             `className` := "toggle-all",
             `type` := "checkbox",
             `style` := "cursor: pointer",
-            `checked` := Bind { todos.length() > 0 && numCompleted() == todos.length() },
+            `checked` := {
+              for {
+                todosLength ← todos.rxLength
+                numCompleted ← numCompleted
+              } yield {
+                todosLength > 0 && numCompleted == todosLength
+              }
+            },
             on.click subscribe { event =>
               `checked` from event.target onSuccess {
                 case true =>
@@ -136,7 +163,7 @@ object MoorkaTodoMVC extends Application {
               Seq(All, Active, Completed).map { x =>
                 li(
                   a(`href`:="#",
-                    useClassName("selected") := Bind { filter() == x },
+                    useClassName("selected") := filter.map(_ == x),
                     on.click listen {
                       filter() = x
                     },
